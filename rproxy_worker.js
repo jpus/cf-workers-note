50777dbe6295d0f16fc79f929158e385a2a22eec
jpus <zengqcyxx@gmail.com>
2024-10-15 22:16:32 +0800

    Update rproxy_worker.js

--- rproxy_worker.js
+++ rproxy_worker.js
@@ -5,11 +5,12 @@ addEventListener("fetch", event => {
 async function handleRequest(request) {
   const url = new URL(request.url)
 
-  // 使用 hostname 设置域名，此选项适合目标主机使用443端口的平台
+  // 此选项适合目标主机域名使用证书443端口的平台
   url.hostname = "xxxxxx.xxxxxx.com"
   
-  // 使用 host 等于域名+端口，此选项适合目标主机使用非标端口的平台
-  // url.host = "xxxxxx.xxxxxx.com:8080"
+  // 此选项适合目标主机不带证书及使用非标端口的平台.cloudflare优选只能使用支持http的80, 8080, 8880, 2052, 2082, 2086, 2095端口
+  // 目标主机不带证书及使用非标端口而想套用cf的证书，不应创建此worker.而是cf托管的域名创建目标主机的A或AAAA或CNAME记录，再创建规则origin-rules端口回源
+  // url.host = "xxxxxx.xxxxxx.com:13579"
   
   // 发起https请求以https代理，发起http请求以http代理
   url.protocol = request.url.startsWith('https') ? 'https:' : 'http:'
